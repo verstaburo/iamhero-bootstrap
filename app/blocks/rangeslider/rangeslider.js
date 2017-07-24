31fdd0861d6e621c09409a6fba614f02a6285529
alege <aleggge@gmail.com>
2017-07-25 00:21:20 +0300

    profile-edit update

---
+++ app/blocks/rangeslider/rangeslider.js
@@ -0,0 +1,28 @@
+export default () => {
+  /*eslint-disable*/
+  var $document = $(document);
+  var $selector = $('input[type="range"]');
+
+  $selector.rangeslider({
+    polyfill: false,
+    rangeClass: 'rangeslider',
+    disabledClass: 'rangeslider_disabled',
+    horizontalClass: 'rangeslider_horizontal',
+    verticalClass: 'rangeslider_vertical',
+    fillClass: 'rangeslider__fill',
+    handleClass: 'rangeslider__handle',
+  });
+
+  function valueOutput(el) {
+    var output = el.parents('.rangeslider-wrap').find('.rangeslider-wrap__output');
+    var value = el.val();
+    output.html(value+'%');
+  }
+
+  valueOutput($selector);
+
+  $selector.on('input', function () {
+    valueOutput($(this));
+  });
+
+};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import objectFitImages from 'object-fit-images';
+import rangeSliderInit from '../blocks/rangeslider/rangeslider';
 
-const $ = window.$;
-
+// eslint-disable-next-line
 $(() => {
   objectFitImages();
   svg4everybody();
+  rangeSliderInit();
 });
