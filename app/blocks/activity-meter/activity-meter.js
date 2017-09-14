328e9fc57da99e05237a1052212ae5531936e3e0
alege <aleggge@gmail.com>
2017-09-14 18:28:52 +0300

    finances static

--- app/blocks/activity-meter/activity-meter.js
+++ app/blocks/activity-meter/activity-meter.js
@@ -1,22 +1,26 @@
 export default () => {
-  let i = 1;
-  const labelsData = [];
+  const ITEM_CLASS = '.activity-meter';
 // eslint-disable-next-line
-  for (i = 1; i <= 30; i++) {
-    labelsData.push(i);
+  if ($(ITEM_CLASS).length) {
+    let i = 1;
+    const labelsData = [];
+  // eslint-disable-next-line
+    for (i = 1; i <= 30; i++) {
+      labelsData.push(i);
+    }
+
+    const data = {
+      labels: labelsData,
+      series: [
+        [48, 56, 66, 54, 36, 30, 56, 60, 60, 51, 51, 56],
+      ],
+    };
+
+    const options = {
+      low: 0,
+      onlyInteger: true,
+    };
+  // eslint-disable-next-line
+    new Chartist.Bar('.activity-meter', data, options);
   }
-
-  const data = {
-    labels: labelsData,
-    series: [
-      [48, 56, 66, 54, 36, 30, 56, 60, 60, 51, 51, 56],
-    ],
-  };
-
-  const options = {
-    low: 0,
-    onlyInteger: true,
-  };
-// eslint-disable-next-line
-  new Chartist.Bar('.activity-meter', data, options);
 };
