48f3e6ceeb979a6a5d0390dbd03361ca4a38035e
alege <aleggge@gmail.com>
2017-10-02 14:55:50 +0300

    invite friends & acc settings adaptive

--- app/components/inner-header/inner-header.js
+++ app/components/inner-header/inner-header.js
@@ -1,11 +1,22 @@
 /*eslint-disable*/
 export default () => {
-  $('.inner-header__col_dropdown').click(function () {
+
+  $(document).on('click', '.inner-header__col_dropdown', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
     $(this).siblings($(this)).removeClass('active');
+    $(this).parents('.inner-header').find('.inner-header__search').removeClass('active');
     $(this).toggleClass('active');
   });
 
-  $('.inner-header__search-btn').click(function () {
+  $(document).on('click', '.inner-header__search-btn', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).siblings('.inner-header__notify-wrap').find('.inner-header__col_dropdown').removeClass('active');
     $(this).siblings('.inner-header__search').toggleClass('active');
-  });
+  })
+
+  $(document).on('click', function () {
+    $('.inner-header__col_dropdown, .inner-header__search').removeClass('active');
+  })
 };
