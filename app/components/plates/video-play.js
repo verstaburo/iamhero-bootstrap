8667e4782d2762fbf316453b89d15b630750fba0
alege <aleggge@gmail.com>
2017-10-02 21:09:52 +0300

    video-play tag fix

--- app/components/plates/video-play.js
+++ app/components/plates/video-play.js
@@ -11,7 +11,7 @@ export default () => {
     if (mediaElement.prop('tagName') === 'IFRAME') {
       mediaElement[0].src += '&autoplay=1';
     } else if (mediaElement.prop('tagName') === 'VIDEO') {
-      mediaElement.play();
+      mediaElement[0].play();
     } else {
       console.log("Can't find video in placeholder");
     }
