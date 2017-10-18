2a0b16d6b40685652c66a515db12ecafe6e2e4ba
alege <aleggge@gmail.com>
2017-10-18 17:02:48 +0300

    radio & checkbox rework

--- app/blocks/checkbox/checkbox.js
+++ app/blocks/checkbox/checkbox.js
@@ -1,7 +1,12 @@
 export default () => {
   /* eslint-disable */
-  $('.checkbox, .checkbox-inline').each(function () {
+  $('.checkbox').each(function () {
     const box = $('<span></span>').addClass('checkbox-box');
     $(this).find('label').append(box);
+  });
+
+  $('.checkbox-inline').each(function () {
+    const box = $('<span></span>').addClass('checkbox-box');
+    $(this).append(box);
   });
 };
--- app/blocks/radio/radio.js
+++ app/blocks/radio/radio.js
@@ -1,6 +1,11 @@
 export default () => { /*eslint-disable*/
-  $('.radio, .radio-inline').each(function () {
+  $('.radio').each(function () {
     const box = $('<span></span>').addClass('radio-box');
     $(this).find('label').append(box);
+  });
+
+  $('.radio-inline').each(function () {
+    const box = $('<span></span>').addClass('radio-box');
+    $(this).append(box);
   });
 };
