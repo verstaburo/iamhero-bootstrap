0c08f951f66d0abd1df884d7162c62049a388058
alege <aleggge@gmail.com>
2017-10-25 11:12:27 +0300

    text resize fix

--- app/scripts/common/fitText.js
+++ app/scripts/common/fitText.js
@@ -1,5 +1,6 @@
 /*eslint-disable*/
-export default() => {
+import { throttle } from 'throttle-debounce';
+export default () => {
   // txt is the text to measure, font is the full CSS font declaration,
   // e.g. "bold 12px Verdana"
   function measureText(txt, font) {
@@ -19,37 +20,43 @@ export default() => {
     }
   }
 
-  window.shrinkToFill =  function (input, fontSize, fontWeight, fontFamily) {
+  window.shrinkToFill = function() {
+    var input = $('.responsive-font');
+
     if (!input.length) {
       return;
     }
-    var $input = $(input),
-      txt = $input.val()||$input.text(),
-      maxWidth = $input.parent().width(), // add some padding
-      font = fontWeight + " " + fontSize + "px " + fontFamily;
-    // see how big the text is at the default size
-    var textWidth = measureText(txt, font).width;
-    if (textWidth > maxWidth) {
-      // if it's too big, calculate a new font size
-      // the extra .9 here makes up for some over-measures
-      fontSize = fontSize * maxWidth / textWidth;
-      font = fontWeight + " " + fontSize + "px " + fontFamily;
-      // and set the style on the input
-      $input.css({
-        font: font
-      });
-    } else {
-      // in case the font size has been set small and
-      // the text was then deleted
-      $input.css({
-        font: font
-      });
-    }
+
+    var fontSize = 53,
+      fontWeight = 100,
+      fontFamily = 'Museo Sans, sans-serif';
+    input.each(function() {
+      var $input = $(this),
+        txt = $input.val() || $input.text(),
+        maxWidth = $input.parent().width(), // add some padding
+        font = fontWeight + " " + fontSize + "px " + fontFamily;
+      // see how big the text is at the default size
+      var textWidth = measureText(txt, font).width;
+      if (textWidth > maxWidth) {
+        // if it's too big, calculate a new font size
+        // the extra .9 here makes up for some over-measures
+        fontSize = fontSize * maxWidth / textWidth;
+        font = fontWeight + " " + fontSize + "px " + fontFamily;
+        // and set the style on the input
+        $input.css({
+          font: font
+        });
+      } else {
+        // in case the font size has been set small and
+        // the text was then deleted
+        $input.css({
+          font: font
+        });
+      }
+    });
   }
 
-  shrinkToFill($('.responsive-font'), 53, 100, "Museo Sans, sans-serif");
+  shrinkToFill();
 
-  $(document).on('input', '.responsive-font', function () {
-    shrinkToFill($(this), 53, 100, "Museo Sans, sans-serif");
-  });
+  $(document).on('input', '.responsive-font', throttle(150, shrinkToFill));
 };
