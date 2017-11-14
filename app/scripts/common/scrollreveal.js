0919b8dfb94ce9c130c2264985e55ec3c7530410
alege <aleggge@gmail.com>
2017-11-15 00:57:44 +0200

    ico landing desktop pre-finel

--- app/scripts/common/scrollreveal.js
+++ app/scripts/common/scrollreveal.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 export default () => {
 
-  if(!$('.sr-animate').length) {
+  if (!$('.sr-animate').length) {
     return
   }
 
@@ -11,4 +11,56 @@ export default () => {
   });
 
   sr.reveal('.sr-animate');
+
+  const mockup1 = {
+    delay: 100,
+    distance: '-400px',
+    easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+    scale: 1,
+    mobile: true,
+    duration: 1000,
+    viewFactor: 0.1,
+  };
+  const mockup2 = {
+    delay: 400,
+    distance: '-400px',
+    easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+    scale: 1,
+    mobile: true,
+    duration: 1000,
+    viewFactor: 0.1,
+  };
+  const mockup3 = {
+    delay: 600,
+    distance: '-400px',
+    easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+    scale: 1,
+    mobile: true,
+    duration: 1000,
+    viewFactor: 0.1,
+  };
+  const mockup4 = {
+    delay: 800,
+    distance: '-400px',
+    easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+    scale: 1,
+    mobile: true,
+    duration: 1000,
+    viewFactor: 0.1,
+  };
+  const mockup5 = {
+    delay: 1000,
+    distance: '-400px',
+    easing: 'cubic-bezier(0.3, 0.5, 0.1, 1)',
+    scale: 1,
+    mobile: true,
+    duration: 1000,
+    viewFactor: 0.1,
+  };
+
+  sr.reveal('.mockup-1', mockup1);
+  sr.reveal('.mockup-2', mockup2);
+  sr.reveal('.mockup-3', mockup3);
+  sr.reveal('.mockup-4', mockup4);
+  sr.reveal('.mockup-5', mockup5);
 };
