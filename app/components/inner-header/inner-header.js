b1df65be372cab03304a0a5d6a218d922e0bb310
alege <aleggge@gmail.com>
2017-11-17 14:42:27 +0200

    ico safari fixes

--- app/components/inner-header/inner-header.js
+++ app/components/inner-header/inner-header.js
@@ -15,13 +15,16 @@ export default () => {
     $(this).siblings('.inner-header__search').toggleClass('active');
   })
 
-  $(document).on('click', '.inner-header__burger', function (e) {
-    e.preventDefault();
-    $(this).toggleClass('active');
-    $('.inner-sidebar').toggleClass('active');
+  $(document).on('click', '.inner-header__burger', function () {
+    $('.inner-header__burger').toggleClass('active');
+    if ($('.inner-sidebar').length) {
+      $('.inner-sidebar').toggleClass('active');
+    }
   })
 
   $(document).on('click', function () {
-    $('.inner-header__col_dropdown').removeClass('active');
+    if ($('.inner-header__col_dropdown').hasClass('active')) {
+      $('.inner-header__col_dropdown').removeClass('active');
+    }
   })
 };
